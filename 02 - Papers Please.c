b0b6f5fea5a8b2e24cccad1b4a1690298c7c436c
Krut Patel <f20170184@hyderabad.bits-pilani.ac.in>
2018-09-26 18:33:26 +0530

    Fix solution for Q2

--- 02 - Papers Please.c
+++ 02 - Papers Please.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct node {
-    int data;
-    struct node* next;
-}node;
-
 int comp_matrix[26][26], inputted[26];
 
 int main(int argc, char const *argv[]) {
@@ -26,74 +20,54 @@ int main(int argc, char const *argv[]) {
             return 0;
         }
         comp_matrix[ind1][ind2] = 1;
+        comp_matrix[ind2][ind1] = -1;
         inputted[ind1]=1;
         inputted[ind2]=1;
     }
     for(int i=0; i<26; i++){
         for(int j=0; j<26; j++){
-            printf("%d ", comp_matrix[i][j]);
-        }
-        printf("\n");
-    }
-    for(int i=0; i<26; i++){
-        for(int j=0; j<26; j++){
-            if(i!=j &&comp_matrix[i][j]){
-                if(comp_matrix[j][i]){
+            if(i!=j &&comp_matrix[i][j]==1){
+                if(comp_matrix[j][i]==1){
                     printf("-1");
                     return 0;
                 }
                 for(int k=0;k<26;k++){
-                    if(comp_matrix[j][k]){
+                    if(comp_matrix[j][k]==1){
                         comp_matrix[i][k]=1;
                     }
                 }
             }
         }
     }
-    printf("\n");
-    for(int i=0; i<26; i++){
-        for(int j=0; j<26; j++){
-            printf("%d ", comp_matrix[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
     for(int i=0; i<26; i++){
         if(!inputted[i])
             continue;
-        for(int j=0; j<26; j++){
-            if(i==j||!inputted[j])
+        for(int j=i+1; j<26; j++){
+            if(!inputted[j])
                 continue;
-            if(!(comp_matrix[i][j]+comp_matrix[j][i]))
-                comp_matrix[i<j?i:j][i<j?j:i]=1;
+            for(int k=25; k>=j; k--){
+                if(comp_matrix[i][k]==-1){
+                    comp_matrix[j][i]=1;
+                }
+            }
         }
     }
-    for(int i=0; i<26; i++){
-        for(int j=0; j<26; j++){
-            printf("%d ", comp_matrix[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    int max=0;
+    int max=0, row_sums[26];
     for(int i=0; i<26; i++){
         int row_sum=0;
         for(int j=0; j<26; j++){
-            row_sum+=comp_matrix[i][j];
+            if(comp_matrix[i][j]==1)
+                row_sum+=comp_matrix[i][j];
         }
-        if(row_sum>max){
+        row_sums[i]=row_sum;
+        if(row_sum>max)
             max=row_sum;
-        }
     }
     for(int s=max;s>=0;s--){
         for(int i=0; i<26; i++){
             if(!inputted[i])
                 continue;
-            int row_sum=0;
-            for(int j=0; j<26; j++){
-                row_sum+=comp_matrix[i][j];
-            }
-            if(row_sum==s){
+            if(row_sums[i]==s){
                 printf("%c ", i+'a');
             }
         }
