96e8815ec1f6fa42a398554fd2e765d8897d99cd
Krut Patel <f20170184@hyderabad.bits-pilani.ac.in>
2018-09-29 12:43:19 +0530

    Add solution for Q5

--- 05 - Immediate refinement.c
+++ 05 - Immediate refinement.c
@@ -1,95 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-typedef struct node {
-    int data;
-    struct node* prev;
-    struct node* next;
-}node;
-
-#define STARTMARKER -999
-#define PARTITIONMARKER -1000
-
-node* create_node(int d){
-    node* new_node = (node*)malloc(sizeof(node));
-    new_node->data = d;
-    new_node->prev = NULL;
-    new_node->next = NULL;
-    return new_node;
+void disparr(int n, int arr[]){
+    for(int i=1; i<=n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
 
-void append_to_list(node** head, int d){
-    node* new_node = create_node(d);
-    if(*head == NULL){
-        *head = new_node;
-        return;
-    }
-    node* cursor = *head;
-    while(cursor->next){
-        cursor = cursor->next;
-    }
-    cursor->next = new_node;
-    new_node->prev = cursor;
-}
-
-void display(node* start){
-    while(start!=NULL){
-        if(start->data==STARTMARKER)
-            printf("{{");
-        else if (start->data==PARTITIONMARKER)
-            printf("\b\b}{");
-        else
-            printf("%d, ", start->data);
-        start=start->next;
-    }
-    printf("\b}\n");
-}
-
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]){
     int n;
     scanf("%d\n", &n);
-    node* start = NULL;
-    append_to_list(&start, STARTMARKER);
+    int a[n][10000];
     for(int i=0; i<n; i++){
         int k;
-        scanf("%d\n", &k);
-        for(int j=0; j<k; j++){
-            int a;
-            scanf("%d ", &a);
-            append_to_list(&start, a);
+        scanf("%d\n", &a[i][0]);
+        for(int j=1; j<=a[i][0]; j++){
+            scanf("%d ", &a[i][j]);
         }
-        append_to_list(&start, PARTITIONMARKER);
     }
-    printf("Original: ");display(start);
-    printf("Possible refinements:\n");
-    node* cursor = start;
-    while(cursor){
-        node* new_cur = cursor->next;
-        while(new_cur&&new_cur->data!=PARTITIONMARKER){
-            node* prev = new_cur->prev;
-            node* next = new_cur->next;
-            if(next&&prev&&(prev->data==STARTMARKER||prev->data==PARTITIONMARKER)&&(next->data==STARTMARKER||next->data==PARTITIONMARKER)){
-                new_cur = new_cur->next;
-                break;
+    for(int i=0; i<n; i++){
+        for(int j=1, max_subsets=pow(2, a[i][0]-1); j<max_subsets; j++){
+            int ss[a[i][0]], l=0, nss[a[i][0]], nl=0;
+            for(int k=1; k<=a[i][0]; k++){
+                if(j&1<<(k-1)){
+                    ss[l]=a[i][k];
+                    l++;
+                }
+                else{
+                    nss[nl]=a[i][k];
+                    nl++;
+                }
             }
-            prev->next = next;
-            node* temp = cursor->next;
-            cursor->next=new_cur;
-            node* empty = create_node(PARTITIONMARKER);
-            new_cur->next = empty;
-            empty->next=temp;
-            if(prev&&prev->prev&&(prev->prev->data==STARTMARKER||prev->prev->data==PARTITIONMARKER)&&next&&(next->data==STARTMARKER||next->data==PARTITIONMARKER)){
+            for(int j=0; j<i; j++){
+                disparr(a[j][0], a[j]);
             }
-            else{
-                display(start);
+            for(int k=0; k<l; k++){
+                printf("%d ", ss[k]);
             }
-            cursor->next = temp;
-            prev->next=new_cur;
-            new_cur->next=next;
-            new_cur=new_cur->next;
-
+            printf("\n");
+            for(int k=0; k<nl; k++){
+                printf("%d ", nss[k]);
+            }
+            printf("\n");
+            for(int j=i+1; j<n; j++){
+                disparr(a[j][0], a[j]);
+            }
+            printf("*** *** ***\n");
         }
-        cursor=new_cur;
     }
     return 0;
 }
