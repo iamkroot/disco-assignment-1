8a8765b851b5471132da0cafbd0d076468600175
Krut Patel <f20170184@hyderabad.bits-pilani.ac.in>
2018-09-30 11:02:55 +0530

    Improve Q4 drastically

--- 04 - Partition for minimum diff of sums.c
+++ 04 - Partition for minimum diff of sums.c
@@ -1,36 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int n, m;
-int sums[1000];
-
-int partition_min(int l, int num_parts){
-    if(num_parts==1){
-        return sums[m-1] - (l>0?sums[l-1]:0);
-    }
-    int min_sum=1e9;
-    for(int i=l; i<=m-num_parts; i++){
-        int s = sums[i] - (l>0?sums[l-1]:0); // sum of current partition (from l to i)
-        int r = partition_min(i+1, num_parts-1);
-        int v = s>r?s:r; // v is max(sum of current part, highest sum in proceeding parts)
-        if(v<min_sum){
-            min_sum=v;
-        }
-    }
-    return min_sum;
-}
-
-int main(int argc, char const *argv[]) {
-    
+int main(int argc, char const *argv[]){
+    int n, m, lo=0, hi=0;
     scanf("%d %d\n", &n, &m);
-    int s=0;
-    for(int i=0;i<m;i++){
-        int a;
-        scanf("%d ", &a);
-        s+=a;
-        sums[i]=s;
+    int a[m];
+    for(int i=0; i<m; i++){
+        scanf("%d\n", &a[i]);
+        if(lo<a[i])
+            lo=a[i];
+        hi+=a[i];
     }
-    int a = partition_min(0, n);
-    printf("%d\n", a);
+    while(lo<hi){
+        int mid = lo + (hi-lo)/2;
+        int teams=0, current_power=0;
+        for(int i=0; i<m; i++){
+            if(current_power+a[i]<=mid){
+                current_power+=a[i];
+            }
+            else{
+                teams++;
+                current_power=a[i];
+            }
+        }
+        if(teams<=n){
+            hi=mid;
+        }
+        else
+            lo=mid+1;
+    }
+    printf("%d\n", lo);
     return 0;
 }
